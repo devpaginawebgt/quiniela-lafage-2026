8f97c51c11ba794b57f0d3fb87133a73199fba38
Dennis Herrera <dennishumher25@hotmail.com>
2026-05-12 15:13:38 -0600

    chore: changed stats names

--- app/Http/Resources/Equipo/EquipoGrupoResource.php
+++ app/Http/Resources/Equipo/EquipoGrupoResource.php
@@ -17,12 +17,12 @@ class EquipoGrupoResource extends JsonResource
     {
         $stats = [];
 
-        $stats[] = [ 'name' => 'PJ', 'value' => $this->partidos_jugados];
-        $stats[] = [ 'name' => 'PG', 'value' => $this->partidos_ganados];
-        $stats[] = [ 'name' => 'PE', 'value' => $this->partidos_empatados];
-        $stats[] = [ 'name' => 'PP', 'value' => $this->partidos_perdidos];
-        $stats[] = [ 'name' => 'GF', 'value' => $this->goles_favor];
-        $stats[] = [ 'name' => 'GC', 'value' => $this->goles_contra];
+        $stats[] = [ 'name' => 'Partidos Jugados',   'value' => $this->partidos_jugados];
+        $stats[] = [ 'name' => 'Partidos Ganados',   'value' => $this->partidos_ganados];
+        $stats[] = [ 'name' => 'Partidos Empatados', 'value' => $this->partidos_empatados];
+        $stats[] = [ 'name' => 'Partidos Perdidos',  'value' => $this->partidos_perdidos];
+        $stats[] = [ 'name' => 'Goles a favor',      'value' => $this->goles_favor];
+        $stats[] = [ 'name' => 'Goles en contra',    'value' => $this->goles_contra];
 
         return [
             'id' => $this->id,
