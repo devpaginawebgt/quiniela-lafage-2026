0b57c8621c368faab8ea6919f85aadccb21c53b2
Dennis Herrera <dennishumher25@hotmail.com>
2026-05-19 15:48:27 -0600

    chore: added duplicate brands validation

--- resources/views/components/brands-slider.blade.php
+++ resources/views/components/brands-slider.blade.php
@@ -6,13 +6,29 @@
 @if($brands->isNotEmpty())
     @php 
         $sliderId = 'brands-swiper-' . uniqid();
+        $duplicate = $brands->count() < 5;
     @endphp
 
     {{-- <h6 class="text-xl text-center font-semibold mb-4">{{ $title }}</h6> --}}
 
     <div class="swiper {{ $sliderId }} w-full max-w-xl">
         <div class="swiper-wrapper py-2">
-            @for($i = 0; $i < 3; $i++)
+
+            @if ($duplicate)    
+                @for($i = 0; $i < 3; $i++)
+                    @foreach($brands as $brand)
+                        <div class="swiper-slide">
+                            <div class="bg-light rounded-xl p-4 flex items-center justify-center h-full shadow-md shadow-zinc-400">
+                                <img
+                                    src="{{ asset($brand->image) }}"
+                                    alt="{{ $brand->name }}"
+                                    class="w-full max-w-35 aspect-6/3 object-contain"
+                                >
+                            </div>
+                        </div>
+                    @endforeach
+                @endfor
+            @else
                 @foreach($brands as $brand)
                     <div class="swiper-slide">
                         <div class="bg-light rounded-xl p-4 flex items-center justify-center h-full shadow-md shadow-zinc-400">
@@ -24,7 +40,7 @@
                         </div>
                     </div>
                 @endforeach
-            @endfor
+            @endif
         </div>
     </div>
     <div class="{{ $sliderId }}-pagination mt-3 flex justify-center"></div>
